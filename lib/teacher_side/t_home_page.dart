b1254792b90bbceaba975414aa8e36cf1fb6c0b3
Utsav Anand <utsavfrutu@gmail.com>
2020-06-06 06:35:08 +0530

    fixes

---
+++ lib/teacher_side/t_home_page.dart
@@ -0,0 +1,20 @@
+//sample file
+import 'package:flutter/material.dart';
+
+class HomePageT extends StatefulWidget {
+  @override
+  _HomePageTState createState() => _HomePageTState();
+}
+
+class _HomePageTState extends State<HomePageT> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
